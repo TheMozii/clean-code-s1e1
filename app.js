8ff8aa837a029751be93ff5341e28c2f2dab0a7e
TheMozii <ruvimungeitis1@gmail.com>
2025-01-29 10:49:41 +0200

    fix: ensure existing tasks are properly bound on page load

--- app.js
+++ app.js
@@ -105,25 +105,8 @@ const bindTaskEvents = (taskListItem, checkBoxEventHandler) => {
 
 addButton.addEventListener("click", addTask);
 
-//cycle over incompleteTaskHolder ul list items
-//for each list item
-for (var i=0; i<incompleteTaskHolder.children.length;i++){
-
-    //bind events to list items chldren(tasksCompleted)
-    bindTaskEvents(incompleteTaskHolder.children[i],taskCompleted);
-}
-
-
-
-
-//cycle over completedTasksHolder ul list items
-for (var i=0; i<completedTasksHolder.children.length;i++){
-    //bind events to list items chldren(tasksIncompleted)
-    bindTaskEvents(completedTasksHolder.children[i],taskIncomplete);
-}
-
-
-
+[...incompleteTaskHolder.children].forEach(item => bindTaskEvents(item, taskCompleted));
+[...completedTasksHolder.children].forEach(item => bindTaskEvents(item, taskIncomplete));
 
 // Issues with usability don't get seen until they are in front of a human tester.
 
