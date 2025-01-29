ce6e2251477b0b18641a5c1cf205c1998ad6c0b6
TheMozii <ruvimungeitis1@gmail.com>
2025-01-29 10:46:18 +0200

    refactor: simplify bindTaskEvents function for better readability

--- app.js
+++ app.js
@@ -93,26 +93,15 @@ const taskIncomplete = function() {
 //The glue to hold it all together.
 
 //Set the click handler to the addTask function.
-addButton.onclick=addTask;
-addButton.addEventListener("click",addTask);
-addButton.addEventListener("click",ajaxRequest);
+const bindTaskEvents = (taskListItem, checkBoxEventHandler) => {
+    const checkBox = taskListItem.querySelector(".todo-list__checkbox");
+    const editButton = taskListItem.querySelector(".todo-list__edit-button");
+    const deleteButton = taskListItem.querySelector(".todo-list__delete-button");
 
-
-var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
-    console.log("bind list item events");
-//select ListItems children
-    var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
-
-
-    //Bind editTask to edit button.
-    editButton.onclick=editTask;
-    //Bind deleteTask to delete button.
-    deleteButton.onclick=deleteTask;
-    //Bind taskCompleted to checkBoxEventHandler.
-    checkBox.onchange=checkBoxEventHandler;
-}
+    editButton.onclick = editTask;
+    deleteButton.onclick = deleteTask;
+    checkBox.onchange = checkBoxEventHandler;
+};
 
 //cycle over incompleteTaskHolder ul list items
 //for each list item
